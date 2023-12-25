d54510a0d04aeea231501595f5866e5ae60129ce
2024kpromote <2024kpromote>
2023-12-25 15:31:15 +0800

    Update

---
+++ precache-manifest.17b71ce4cbad05ca61259723b39e600a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506b84fd7b9ac9f80cb5db399c5a1708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86155dfc2c856a4853bd",
+    "url": "/static/css/main.6da77629.chunk.css"
+  },
+  {
+    "revision": "22306615d9a3df7af8ee",
+    "url": "/static/js/2.3630dea4.chunk.js"
+  },
+  {
+    "revision": "86155dfc2c856a4853bd",
+    "url": "/static/js/main.397ef438.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
